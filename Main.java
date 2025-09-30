2952a7c97f42c4999fb5bebafe27aee21880ccf3
Ayesha Abid <abidayesha428@gmail.com>
2025-09-30 16:26:27 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+      public class Main{
+       public static void main(String[] args) {
+           Turtle t = new Turtle();
+           t.setColour();
+           t.pendown();
+           t.move();
+           t.TurnLeft();
+           t.TurnRight();
+           t.display();
+       }
+}
