dbfcc0a1ca14fcdd8b976ba50f3ea2b330eeae02
Ayesha Abid <abidayesha428@gmail.com>
2025-09-30 16:25:11 +0500

    Update Turtle.java

--- Turtle.java
+++ Turtle.java
@@ -49,17 +49,7 @@ public class Turtle extends pen{
        public void display(){
         System.out.println("Position of Turtle:" +x+ "," +y );
        }
-
-
-       //main.java
-       public static void main(String[] args) {
-           Turtle t = new Turtle();
-           t.setColour();
-           t.pendown();
-           t.move();
-           t.TurnLeft();
-           t.TurnRight();
-           t.display();
-       }
-
 }
+
+    
+
